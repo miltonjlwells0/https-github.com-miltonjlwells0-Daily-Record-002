c211b46d1c89b9dd0e64acbaed5b994a276cdd71
miltonjlwells0 <miltonjlwells0@gmail.com>
2026-08-18 22:47:20 -1200

    Add Android offline foundation smoke test

---
+++ app/src/test/java/com/dailyrecord/app/data/RepositorySmokeTest.kt
@@ -0,0 +1,11 @@
+package com.dailyrecord.app.data
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class RepositorySmokeTest {
+    @Test
+    fun offlineFoundationTestSuiteIsEnabled() {
+        assertEquals(2, 1 + 1)
+    }
+}
